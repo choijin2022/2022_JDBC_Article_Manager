f0ee0acf631f22664ab61ebaf8a22cda2559936a
choijin2022 <choeji2022@gmail.com>
2022-10-05 19:50:58 +0900

    Article Manager 기본 구조 작성

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.example.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +9,8 @@ public class Main {
 
 		System.out.println("프로그램 시작");
 		Scanner sc = new Scanner(System.in);
-
+		List<Article> articles = new ArrayList<>();
+		int lastArticleId=0;
 		while (true) {
 			System.out.printf("명령어 > ");
 			String cmd = sc.nextLine().trim();
@@ -16,6 +19,38 @@ public class Main {
 				System.out.println("프로그램 종료");
 				break;
 			}
+			else if(cmd.equals("article write")) {
+				System.out.println(" == 게시물 작성 ==");
+				
+				int id = lastArticleId+1;
+				
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(id,title, body);
+				System.out.println(article);
+				articles.add(article);
+				lastArticleId++;
+				
+				System.out.printf(" %d 번 글이 생성되었습니다.\n",id );
+			}
+			else if(cmd.equals("article list")) {
+				System.out.println(" == 게시물 리스트 ==");
+				
+				if(articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				System.out.println("id     |       제목");
+				for(Article article : articles) {
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+				
+			}
+			
+			
 
 			
 
