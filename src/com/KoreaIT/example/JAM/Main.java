b0a9399aef4bf07018cec676867e12145ab3f58c
choijin2022 <choeji2022@gmail.com>
2022-10-05 21:48:31 +0900

    jdbc mysql isert test

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,5 +1,9 @@
 package com.KoreaIT.example.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -29,10 +33,56 @@ public class Main {
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(id,title, body);
-				System.out.println(article);
-				articles.add(article);
-				lastArticleId++;
+				//sql 작성?
+				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/jdbc_article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					// title 
+					sql += ", title = CONCAT('제목', RAND())";
+					// body
+					sql += ", `body` = CONCAT('내용', RAND());";
+
+					pstmt = conn.prepareStatement(sql);
+
+					pstmt.executeUpdate();
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				//
+//				Article article = new Article(id,title, body);
+//				System.out.println(article);
+//				articles.add(article);
+//				lastArticleId++;
+				
 				
 				System.out.printf(" %d 번 글이 생성되었습니다.\n",id );
 			}
