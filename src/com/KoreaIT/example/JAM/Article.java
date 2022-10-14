52c96fdabdf3dc3cbf36b694041dc30dc050047e
choijin2022 <choeji2022@gmail.com>
2022-10-14 21:14:55 +0900

    MVC 모델 구현

--- src/com/KoreaIT/example/JAM/Article.java
+++ src/com/KoreaIT/example/JAM/Article.java
@@ -4,17 +4,11 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 public class Article extends Object {
-	int id;
-	LocalDateTime regDate;
-	LocalDateTime updateDate;
-	String title;
-	String body;
-	
-	public Article(int id, String title, String body) {
-		this.id = id;
-		this.title = title;
-		this.body = body;
-	}
+	public int id;
+	public LocalDateTime regDate;
+	public LocalDateTime updateDate;
+	public String title;
+	public String body;
 
 	public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String title, String body) {
 		this.id = id;
@@ -25,12 +19,11 @@ public class Article extends Object {
 	}
 
 	public Article(Map<String, Object> articleMap) {
-		// 컬럼명이 key, 가져온 데이터 타입은 위에 명시된 Object 타입 --> 형변환 필요
-		this.id = (int)articleMap.get("id");
-		this.regDate = (LocalDateTime)articleMap.get("regDate");
-		this.updateDate = (LocalDateTime)articleMap.get("updateDate");
-		this.title = (String)articleMap.get("title");
-		this.body = (String)articleMap.get("body");
+		this.id = (int) articleMap.get("id");
+		this.regDate = (LocalDateTime) articleMap.get("regDate");
+		this.updateDate = (LocalDateTime) articleMap.get("updateDate");
+		this.title = (String) articleMap.get("title");
+		this.body = (String) articleMap.get("body");
 	}
 
 	@Override
--- src/com/KoreaIT/example/JAM/controller/MemberController.java
+++ src/com/KoreaIT/example/JAM/controller/MemberController.java
@@ -3,102 +3,87 @@ package com.KoreaIT.example.JAM.controller;
 import java.sql.Connection;
 import java.util.Scanner;
 
-import com.KoreaIT.example.JAM.util.DBUtil;
-import com.KoreaIT.example.JAM.util.SecSql;
+import com.KoreaIT.example.JAM.service.MemberService;
 
-public class MemberController {
-	
-	
+public class MemberController extends Controller {
+
+	private MemberService memberService;
 	
 	public MemberController(Connection conn, Scanner sc) {
-		// TODO Auto-generated constructor stub
+		super(sc);
+		this.memberService = new MemberService(conn);
 	}
 
-	public void doJoin(cmd){
+	public void doJoin(String cmd) {
 		String loginId = null;
 		String loginPw = null;
 		String loginPwChk = null;
 		String name = null;
 		System.out.println("== 회원가입 ==");
-		
-		while(true) {
+
+		while (true) {
 			System.out.printf("아이디 : ");
 			loginId = sc.nextLine().trim();
-			
-			if(loginId.length() == 0) {
+
+			if (loginId.length() == 0) {
 				System.out.println("아이디를 입력해주세요");
 				continue;
 			}
 			
-			// 중복체크
-			SecSql sql = new SecSql();
-			sql.append("SELECT COUNT(loginId) > 0");
-			sql.append("FROM `member`");
-			sql.append("WHERE loginId = ?",loginId);
-			
-			boolean isLoginIdDup = DBUtil.selectRowBooleanValue(conn, sql);
-			// true는 중복된 id가 존재함
-			if(isLoginIdDup) {
-				System.out.printf("%s은(는) 이미 사용중인 아이디입니다.\n", loginId);
+			boolean isLoginIdDup = memberService.isLoginIdDup(loginId);
+
+			if (isLoginIdDup) {
+				System.out.printf("%s은(는) 이미 사용중인 아이디입니다\n", loginId);
 				continue;
 			}
-			
+
 			break;
 		}
-		while(true) {
+		while (true) {
 			System.out.printf("비밀번호 : ");
 			loginPw = sc.nextLine().trim();
-			
-			if(loginPw.length() == 0) {
+
+			if (loginPw.length() == 0) {
 				System.out.println("비밀번호를 입력해주세요");
 				continue;
 			}
-			
+
 			boolean loginPwCheck = true;
-			
-			while(true) {
+
+			while (true) {
 				System.out.printf("비밀번호 확인 : ");
 				loginPwChk = sc.nextLine().trim();
-				
-				if(loginPwChk.length() == 0) {
+
+				if (loginPwChk.length() == 0) {
 					System.out.println("비밀번호 확인을 입력해주세요");
 					continue;
 				}
-				
-				if(loginPw.equals(loginPwChk) == false) {
+
+				if (loginPw.equals(loginPwChk) == false) {
 					System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요");
 					loginPwCheck = false;
 				}
 				break;
 			}
-			if(loginPwCheck) {
+			if (loginPwCheck) {
 				break;
 			}
 		}
-		
-		while(true) {
+
+		while (true) {
 			System.out.printf("이름 : ");
 			name = sc.nextLine().trim();
-			
-			if(name.length() == 0) {
+
+			if (name.length() == 0) {
 				System.out.println("이름을 입력해주세요");
 				continue;
 			}
 			break;
 		}
+		
+		memberService.doJoin(loginId, loginPw, name);
 
-		SecSql sql = new SecSql();
-
-		sql.append("INSERT INTO `member`");
-		sql.append("SET regDate = NOW()");
-		sql.append(", updateDate = NOW()");
-		sql.append(", loginId = ?", loginId);
-		sql.append(", loginPw = ?", loginPw);
-		sql.append(", `name` = ?", name);
-
-		DBUtil.insert(conn, sql);
-
-		System.out.printf("%s님, 가입 되었습니다\n", name);
-
+		System.out.printf("%s 님, 가입 되었습니다\n", name);
 	}
+	
 }
